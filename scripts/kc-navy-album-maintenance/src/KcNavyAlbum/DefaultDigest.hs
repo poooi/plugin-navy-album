5dc70a2d78da69fccb04d7cbf83bc08bd1e513b2
Javran Cheng <Javran.C@gmail.com>
2021-10-31 23:28:22 -0700

    handle JSON parsing with kantour-core.
    should simplify scripting part a bit.

--- scripts/kc-navy-album-maintenance/src/KcNavyAlbum/DefaultDigest.hs
+++ scripts/kc-navy-album-maintenance/src/KcNavyAlbum/DefaultDigest.hs
@@ -1,12 +1,8 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE DeriveGeneric #-}
-{-# LANGUAGE DerivingVia #-}
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE TypeApplications #-}
-{-# LANGUAGE TypeOperators #-}
 
 module KcNavyAlbum.DefaultDigest where
 
@@ -17,73 +13,25 @@ import Data.List
 import qualified Data.List.NonEmpty as NE
 import qualified Data.Text as T
 import qualified Data.Vector as V
-import Deriving.Aeson
+import Kantour.Core.KcData.Master.Root
+import Kantour.Core.KcData.Master.Ship
+import Kantour.Core.KcData.Master.Shipgraph
+import Kantour.Core.KcData.Master.Slotitem
 import Network.HTTP.Client
 import System.Exit
-
-data KcApiField
-
-instance StringModifier KcApiField where
-  getStringModifier = ("api_" <>)
-
-type KcConvention = [CamelToSnake, KcApiField]
-
-data KcMasterData = KcMasterData
-  { mstSlotitem :: [KcSlotitem]
-  , mstShipgraph :: [KcShipgraph]
-  , mstShip :: [KcShip]
-  }
-  deriving stock (Generic, Show)
-  deriving
-    (FromJSON)
-    via CustomJSON
-          '[FieldLabelModifier KcConvention]
-          KcMasterData
-
-data KcSlotitem = KcSlotitem
-  { slotId :: Int
-  }
-  deriving stock (Generic, Show)
-  deriving
-    (FromJSON)
-    via CustomJSON
-          '[FieldLabelModifier (Rename "slotId" "id" : KcConvention)]
-          KcSlotitem
-
-data KcShipgraph = KcShipgraph
-  { version :: NE.NonEmpty T.Text
-  , filename :: T.Text
-  , shipId :: Int
-  }
-  deriving stock (Generic, Show)
-  deriving
-    (FromJSON)
-    via CustomJSON
-          '[FieldLabelModifier (Rename "shipId" "id" : KcConvention)]
-          KcShipgraph
-
-data KcShip = KcShip
-  { shipId :: Int
-  }
-  deriving stock (Generic, Show)
-  deriving
-    (FromJSON)
-    via CustomJSON
-          '[FieldLabelModifier (Rename "shipId" "id" : KcConvention)]
-          KcShip
 
 subCmdMain :: Manager -> String -> IO ()
 subCmdMain mgr _cmdHelpPrefix = do
   req <- parseRequest "https://raw.githubusercontent.com/kcwiki/kancolle-data/master/api/api_start2.json"
   resp <- httpLbs req mgr
-  parsed <- case Aeson.eitherDecode @KcMasterData (responseBody resp) of
+  parsed <- case Aeson.eitherDecode @MasterRoot (responseBody resp) of
     Left msg -> die ("parse error: " <> msg)
     Right r -> pure r
   encodeFile "assets/default-digest.json" (mkDigest parsed)
   putStrLn "Written to default-digest.json."
 
-mkDigest :: KcMasterData -> Value
-mkDigest KcMasterData {mstSlotitem, mstShipgraph, mstShip} =
+mkDigest :: MasterRoot -> Value
+mkDigest MasterRoot {mstSlotitem, mstShipgraph, mstShip} =
   Object $
     HM.fromList
       [ ( "equipMstIds"
@@ -102,12 +50,12 @@ mkDigest KcMasterData {mstSlotitem, mstShipgraph, mstShip} =
                        [ Number (fromIntegral shipId)
                        , String (graphDigestTable IM.! shipId)
                        ])
-                $ sort $ fmap (\KcShip {shipId} -> shipId) mstShip
+                $ sort $ fmap (\Ship {shipId} -> shipId) mstShip
         )
       ]
   where
     graphDigestTable :: IM.IntMap T.Text
     graphDigestTable = IM.fromList $ fmap mk mstShipgraph
       where
-        mk KcShipgraph {shipId, filename, version} =
+        mk Shipgraph {shipId, filename, version} =
           (shipId, filename <> "#" <> NE.head version)
