e85f28502d787d3ee29b16c60403f2c10dcb206a
Javran Cheng <Javran.C@gmail.com>
2021-11-11 21:23:39 -0800

    simplify main module.

--- scripts/kc-navy-album-maintenance/src/KcNavyAlbum/CmdCommon.hs
+++ scripts/kc-navy-album-maintenance/src/KcNavyAlbum/CmdCommon.hs
@@ -1,10 +1,21 @@
+{-# LANGUAGE NamedFieldPuns #-}
+{-# LANGUAGE TypeApplications #-}
+
 module KcNavyAlbum.CmdCommon
   ( CmdCommon (..)
+  , mkCmdCommon
   )
 where
 
+import Control.Once
+import qualified Data.Aeson as Aeson
+import Kantour.Core.GameResource.Magic
 import Kantour.Core.KcData.Master.Root
 import Network.HTTP.Client
+import Network.HTTP.Client.TLS
+import Network.HTTP.Types
+import System.Exit
+import Text.Printf
 
 data CmdCommon = CmdCommon
   { getManager :: IO Manager
@@ -12,3 +23,21 @@ data CmdCommon = CmdCommon
   , doesResourceExist :: String -> IO Bool
   }
 
+mkCmdCommon :: IO CmdCommon
+mkCmdCommon = do
+  getManager <- once (newManager tlsManagerSettings)
+  getMasterRoot <- once $ do
+    mgr <- getManager
+    req <- parseRequest "https://raw.githubusercontent.com/kcwiki/kancolle-data/master/api/api_start2.json"
+    resp <- httpLbs req mgr
+    case Aeson.eitherDecode @MasterRoot (responseBody resp) of
+      Left msg -> die ("parse error: " <> msg)
+      Right r -> pure r
+
+  let doesResourceExist path = do
+        mgr <- getManager
+        req <- parseRequest (printf "http://%s%s" defaultServer path)
+        resp <- httpNoBody req mgr
+        pure $ statusCode (responseStatus resp) == 200
+
+  pure $ CmdCommon {getManager, getMasterRoot, doesResourceExist}
--- scripts/kc-navy-album-maintenance/src/KcNavyAlbum/Main.hs
+++ scripts/kc-navy-album-maintenance/src/KcNavyAlbum/Main.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TypeApplications #-}
 
 module KcNavyAlbum.Main
   ( main
@@ -16,24 +14,17 @@ where
  -}
 
 import Control.Monad
-import Control.Once
-import qualified Data.Aeson as Aeson
 import qualified Data.Text as T
 import Filesystem.Path.CurrentOS hiding (null)
-import Kantour.Core.KcData.Master.Root
 import KcNavyAlbum.CmdCommon
 import qualified KcNavyAlbum.DefaultDigest
 import qualified KcNavyAlbum.MapBgm
 import qualified KcNavyAlbum.ScanSpecialShipGraphs
-import Network.HTTP.Client
-import Network.HTTP.Client.TLS
-import Network.HTTP.Types
 import System.Environment
 import System.Exit
 import Turtle.Prelude hiding (die)
 import Prelude hiding (FilePath)
-import Text.Printf
-import Kantour.Core.GameResource.Magic
+
 updateKcReplay :: IO ()
 updateKcReplay = do
   (ec, out) <- procStrict "npm" ["run", "update-kcreplay"] ""
@@ -47,25 +38,7 @@ main :: IO ()
 main = do
   Just fp <- need "NAVY_ALBUM_REPO"
   cd (fromText fp)
-
-  common <- do
-    getManager <- once (newManager tlsManagerSettings)
-    getMasterRoot <- once $ do
-      mgr <- getManager
-      req <- parseRequest "https://raw.githubusercontent.com/kcwiki/kancolle-data/master/api/api_start2.json"
-      resp <- httpLbs req mgr
-      case Aeson.eitherDecode @MasterRoot (responseBody resp) of
-        Left msg -> die ("parse error: " <> msg)
-        Right r -> pure r
-
-    let doesResourceExist path = do
-          mgr <- getManager
-          req <- parseRequest (printf "http://%s%s" defaultServer path)
-          resp <- httpNoBody req mgr
-          pure $ statusCode (responseStatus resp) == 200
-
-    pure $ CmdCommon {getManager, getMasterRoot, doesResourceExist}
-
+  common <- mkCmdCommon
   getArgs >>= \case
     subCmd : args
       | Just handler <- lookup subCmd handlers ->
